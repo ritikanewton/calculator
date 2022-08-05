15a40bcdd51ebba6be7fa4a3d1bcce77e0fda266
RITIKA SHARMA <ritika.sharma@newtonschool.co>
2022-08-05 11:05:09 +0530

    changes for controller

--- src/main/java/com/newtonschool/calculator/controllers/CalculatorController.java
+++ src/main/java/com/newtonschool/calculator/controllers/CalculatorController.java
@@ -22,41 +22,8 @@ public class CalculatorController {
     }
 
     private final AtomicLong counter = new AtomicLong();
-    private String badMathMessage = "make sure to provide your equation in the parameter ?calculation=2*2. use %2B for +, i.e. 2%2B2 equals 4";
 
-//    @GetMapping("/calculate")
-//    @ResponseBody
-//    public ResponseEntity<Answer> calculate(@RequestParam(value = "calculation", defaultValue = "") String calculation,
-//                                            @RequestParam(value = "id", defaultValue = "") String id) {
-//        try {
-//            if (!calculation.equals(null) && !calculation.equals("")) {
-//                String newCalc = calculation;
-//                if (id != null && !id.equals("")) {
-//                    String existingValue = JsonUtil.getString(id);
-//                    newCalc = existingValue + newCalc;
-//                }
-//                double result = MathService.calculate(newCalc);
-//                long resultId = counter.incrementAndGet();
-//                Answer answer = new Answer(resultId, result, newCalc);
-//                JsonUtil.setString(Long.toString(resultId), Double.toString(result));
-//                return new ResponseEntity<>(answer, HttpStatus.OK);
-//            } else {
-//                String history = JsonUtil.getAll();
-//                Answer answer = new Answer(0, 0, history);
-//                return new ResponseEntity<>(answer, HttpStatus.BAD_REQUEST);
-//            }
-//        } catch (ScriptException ex) { // not sure I like how this is done
-//            System.out.print(ex);
-//            Answer answer = new Answer(0, 0, badMathMessage);
-//            return new ResponseEntity<>(answer, HttpStatus.BAD_REQUEST);
-//        } catch (Exception ex) {
-//            System.out.print(ex);
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-
-
-    @PostMapping(value = "/sample",
+    @PostMapping(value = "/calculate",
             consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<Answer> sample(@RequestParam(value = "operator", required = true, defaultValue = "") String operator,
